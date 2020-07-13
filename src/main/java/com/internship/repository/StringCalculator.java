acc10687b15d6b2a0eb51beee479c8ead9a49426
aleksandermpiekarz <48812006+aleksandermpiekarz@users.noreply.github.com>
2020-07-13 23:08:03 +0200

    Update StringCalculator.java

--- src/main/java/com/internship/repository/StringCalculator.java
+++ src/main/java/com/internship/repository/StringCalculator.java
@@ -18,18 +18,12 @@ class StringCalculator {
         String[] delimiters = null;
         int sum;
 
-        // here add method looks for delimiters. It can be in the form of //delimiter\n or //[d1][d2]\n
-        // if there is no optional first line with delimiters, standard "," and "\n" delimiter is used
         delimiter = Optional.ofNullable(StringUtils
                 .substringBetween(numbers, "//", "\n"))
                 .orElse(",|\\n");
 
-        // getting rid of the first line ( if exist )
-        numbers = StringUtils.remove(numbers,"//" + delimiter + "\n");
+         numbers = StringUtils.remove(numbers,"//" + delimiter + "\n");
 
-        // app method jumps into this method if first line is in the form //[d1][d2]\n
-        // then were simply using substringsBetween instead of substringBetween ( plural is the difference ) to
-        // get multiple delimiters closed between square brackets
         if(delimiter.contains("[")&&delimiter.contains("]")) {
             delimiters = StringUtils
                     .substringsBetween(delimiter, "[", "]");
@@ -43,13 +37,9 @@ class StringCalculator {
         if(numbers.isEmpty()){ // if string is empty, method returns 0;
             return 0;
 
-            // if string contains some separator it means, there are at least two numbers
-            // ",","\n" and single delimiter are left untouched to ensure backward compatibility for previous tasks
-        }else if(StringUtils.indexOfAny(numbers, delimiters)!=-1) {
+       }else if(StringUtils.indexOfAny(numbers, delimiters)!=-1) {
 
-            // Arrays.asList() is an array with numbers separated by ",", stream is used to
-            // convert string array into integer list.
-            sepNumbers = Arrays.asList(numbers.split(delimiter))
+           sepNumbers = Arrays.asList(numbers.split(delimiter))
                     .stream()
                     .map(s -> Integer.parseInt(s))
                     .collect(Collectors.toList());
@@ -61,8 +51,7 @@ class StringCalculator {
                 return -1;
             }
 
-            // Here again stream is used to filter values below 1000, and calculate sum of that elements
-            sum = sepNumbers
+           sum = sepNumbers
                     .stream().mapToInt(Integer::intValue).filter(i -> i <=1000)
                     .sum();
             return sum;
@@ -83,8 +72,6 @@ class StringCalculator {
         }
     }
 
-    // this function takes table of delimiters, and transforms into form table[0]|table[1] and so on
-    // function toScan is used to put multiple arguments to .split() function
     private String toScan(String[] strings) {
         String finalString = "";
 
